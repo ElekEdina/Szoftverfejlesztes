b5129ead38a098c42c849cedcbcee6214afa4b8f
ElekEdina <71335700+ElekEdina@users.noreply.github.com>
2020-11-08 16:50:37 +0100

    okt 13 dolgozat javitott

---
+++ Dolgozat/dolgozat okt 13/dolgozat/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.matrix2 = new System.Windows.Forms.DataGridView();
+            this.matrix1 = new System.Windows.Forms.DataGridView();
+            this.lbl = new System.Windows.Forms.Label();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.btnGen);
+            this.panel1.Controls.Add(this.matrix2);
+            this.panel1.Controls.Add(this.matrix1);
+            this.panel1.Controls.Add(this.lbl);
+            this.panel1.Controls.Add(this.nud1);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 454);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(366, 79);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 4;
+            this.btnGen.Text = "OK";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // matrix2
+            // 
+            this.matrix2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix2.Location = new System.Drawing.Point(476, 164);
+            this.matrix2.Name = "matrix2";
+            this.matrix2.Size = new System.Drawing.Size(240, 150);
+            this.matrix2.TabIndex = 3;
+            // 
+            // matrix1
+            // 
+            this.matrix1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix1.Location = new System.Drawing.Point(90, 164);
+            this.matrix1.Name = "matrix1";
+            this.matrix1.Size = new System.Drawing.Size(240, 150);
+            this.matrix1.TabIndex = 2;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(119, 39);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(172, 13);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Adjon meg egy számot 5-15 között!";
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(139, 79);
+            this.nud1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nud1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(120, 20);
+            this.nud1.TabIndex = 0;
+            this.nud1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(352, 343);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.DataGridView matrix2;
+        private System.Windows.Forms.DataGridView matrix1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Dolgozat/dolgozat okt 13/dolgozat/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form 
+
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+              
+        private void AdatokFeltolt1(int sor, int oszlop)
+        {
+            matrix1.Columns.Clear();
+            matrix1.Rows.Clear();
+           
+
+            
+            for (int i = 0; i < oszlop; i++)
+            {
+                matrix1.Columns.Add(String.Empty, String.Empty);
+                matrix1.Columns[i].Width = 35;
+                
+            }
+
+            
+            for (int i = 0; i < sor; i++)
+            {
+                object[] intTomb = new object[oszlop]; 
+                for (int j = 0; j < oszlop; j++)
+                {
+                    intTomb[j] = random.Next(-10, 10); 
+                }
+                matrix1.Rows.Add(intTomb);
+                
+            }
+        }
+
+        private void AdatokFeltolt2(int sor, int oszlop)
+        {
+           
+            matrix2.Columns.Clear();
+            matrix2.Rows.Clear();
+
+
+            for (int i = 0; i < oszlop; i++)
+            {
+                
+                matrix2.Columns.Add(String.Empty, String.Empty);
+                matrix2.Columns[i].Width = 35;
+            }
+
+
+            for (int i = 0; i < sor; i++)
+            {
+                object[] intTomb = new object[oszlop];
+                for (int j = 0; j < oszlop; j++)
+                {
+                    intTomb[j] = random.Next(-10, 10);
+                }
+                
+                matrix2.Rows.Add(intTomb);
+            }
+        }
+
+        private int skalaris()
+        {
+            int skalarOsszeg = 0;
+            int sor = Convert.ToInt32(nud1.Value);
+
+            for (int i = 0; i < sor; i++)
+            {
+                for (int j = 0; j < sor; j++)
+                {
+                    skalarOsszeg += Convert.ToInt32(matrix1[i, j].Value) * Convert.ToInt32(matrix2[i, j].Value);
+                }
+            }
+            return skalarOsszeg;
+        }
+
+        private int skalarisvissza() 
+        {
+            int skalarOsszeg = 0;
+            int sor = Convert.ToInt32(nud1.Value);
+            int matrix2sor = sor;
+            int matrix2oszlop = sor;
+
+            for (int i = 0; i < sor; i++)
+            {
+                for (int j = 0; j < sor; j++)
+                {
+                    skalarOsszeg += Convert.ToInt32(matrix1[i, j].Value) * Convert.ToInt32(matrix2[matrix2sor, matrix2oszlop].Value);
+                    
+                    matrix2sor--;
+                }
+                matrix2sor = sor;
+                matrix2oszlop--;
+            }
+            return skalarOsszeg;
+        }
+
+        private int skalarAtlo() 
+        {
+            int skalarOsszeg = 0;
+            int sor = Convert.ToInt32(nud1.Value);
+            int matrix1AtloOsszeg = 0;
+            int matrix2AtloOsszeg = 0;
+
+            for (int i = 0; i < sor; i++)
+            {
+                matrix1AtloOsszeg += 
+                   Convert.ToInt32(matrix1[i,i].Value);
+            }
+
+            for (int i = 0; i < sor; i++)
+            {
+                matrix2AtloOsszeg += Convert.ToInt32(matrix1[i, i].Value);
+            }
+
+            skalarOsszeg = matrix1AtloOsszeg - matrix2AtloOsszeg;
+            return skalarOsszeg;
+        }
+
+       
+
+        private int skalarAtloFelett() 
+        {
+            int skalarOsszeg = 0;
+            int sor = Convert.ToInt32(nud1.Value);
+
+
+            for (int i = 1; i < sor; i++)
+            {
+                skalarOsszeg += Convert.ToInt32(matrix1[i - 1, i].Value); 
+            }
+            return skalarOsszeg;
+        }
+
+        private void EredmenyKiir()
+        {
+            richTextBox1.ResetText();
+            if (skalaris() > 0) { richTextBox1.ForeColor = Color.Blue; }
+            else { richTextBox1.
+                    ForeColor = Color.Red; }
+            richTextBox1.AppendText(String.Format("Eredmény{0}\n", skalaris())); 
+
+
+
+            if (skalaris() > 0) { richTextBox1.ForeColor = Color.Blue; }
+            else
+            {
+                richTextBox1.
+                 ForeColor = Color.Red;
+            }
+            /*richTextBox1.AppendText(String.Format("Eredmény{0}\n", skalarisvissza()));
+
+            if (skalaris() > 0) { richTextBox1.ForeColor = Color.Blue; }
+            else
+            {
+                richTextBox1.
+                 ForeColor = Color.Red;
+            }
+            richTextBox1.AppendText(String.Format("Eredmény{0}\n", skalarAtlo()));*/
+
+            if (skalaris() > 0) { richTextBox1.ForeColor = Color.Blue; }
+            else
+            {
+                richTextBox1.
+                 ForeColor = Color.Red;
+            }
+            richTextBox1.AppendText(String.Format("Eredmény{0}\n", skalarAtloFelett()));
+
+
+        }
+
+
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+
+            AdatokFeltolt1(Convert.ToInt32(nud1.Value), Convert.ToInt32(nud1.Value));
+            AdatokFeltolt2(Convert.ToInt32(nud1.Value), Convert.ToInt32(nud1.Value));
+            EredmenyKiir();
+        }
+    }
+
+   
+        }
+    
